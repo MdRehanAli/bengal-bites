5062741838960c006d33cbedfdae9f0d50a3c8ab
Md Rehan Ali <rehanali21d@gmail.com>
2024-05-27 03:05:06 +0600

    Testimonial Added

---
+++ bengal-bites-client/src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,56 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { useEffect, useState } from "react";
+
+import { Rating } from "@smastrom/react-rating";
+
+import '@smastrom/react-rating/style.css'
+
+
+const Testimonials = () => {
+    const [reviews, setReviews] = useState([])
+
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+
+    return (
+        <section className="my-20">
+            <SectionTitle
+                subHeading={"What Our Client Say"}
+                heading={"Testimonials"}
+            ></SectionTitle>
+
+            <div>
+                <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+
+                    {
+                        reviews.map(review => <SwiperSlide
+                            key={review._id}
+                            review={review}
+                        >
+                            <div className="text-center m-24 flex flex-col items-center mx-24 my-16">
+                                <Rating
+                                    style={{ maxWidth: 180 }}
+                                    value={review.rating}
+                                    onChange={review.setRating}
+                                />
+                                <p className="my-8">{review.details}</p>
+                                <p className="text-2xl text-orange-400">{review.name}</p>
+                            </div>
+                        </SwiperSlide>)
+                    }
+                </Swiper>
+            </div>
+        </section>
+    );
+};
+
+export default Testimonials;
